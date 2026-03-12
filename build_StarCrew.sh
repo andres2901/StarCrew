2f07f5db39d21bcfb2a9011bb16df468941a8f91
andres2901 <af.lizcano@uniandes.edu.co>
2026-03-12 18:07:11 +0100

    solve issue with incompatibilities with updated R packages version

--- build_StarCrew.sh
+++ build_StarCrew.sh
@@ -10,7 +10,7 @@
 # VERSION:     1.0.0
 # ==============================================================================
 
-set -o pipefail
+set -uo pipefail
 
 # ==============================================================================
 # CONSTANTS
@@ -105,7 +105,11 @@ install_r_packages() {
   log_info "Installing gggenomes and verifying required R packages..."
 
   Rscript -e '
-    install.packages("gggenomes", repos = "https://cloud.r-project.org/")
+    install.packages("remotes", repos = "https://cloud.r-project.org/")
+    library(remotes)
+    install_version("gggenomes", version = "1.1.2", repos = "https://cloud.r-project.org/")
+    install_version("ggplot2", version = "3.5.2", repos = "https://cloud.r-project.org/")
+    install_version("dplyr", version = "1.1.4", repos = "https://cloud.r-project.org/")
 
     required_pkgs <- c(
       "optparse", "ggplot2", "ape", "ggtree", "reshape2",
@@ -184,11 +188,7 @@ install_databases() {
   wget "${INTERPROSCAN_URL}"
   wget "${INTERPROSCAN_URL}.md5"
 
-  if ! md5sum -c --quiet ${INTERPROSCAN_ARCHIVE}.md5; then
-    log_warning "pfam installation was unsuccessful"
-    exit 1
-  fi
-
+  md5sum -c "${INTERPROSCAN_ARCHIVE}.md5"
   tar -pxvzf "${INTERPROSCAN_ARCHIVE}" -C "${MAIN_DIR}/interproscan" --strip-components=1
   rm "${INTERPROSCAN_ARCHIVE}"*
 
@@ -228,11 +228,6 @@ install_databases() {
   log_info "Downloading HH-suite pfam database..."
   wget "https://wwwuser.gwdguser.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pfamA_35.0.tar.gz"
   tar -zxvf pfamA_35.0.tar.gz
-
-  if ! md5sum -c --quiet pfam.md5sum; then
-    log_warning "pfam installation was unsuccessful"
-    exit 1
-  fi
   rm pfamA_35.0.tar.gz
 
   cd "${MAIN_DIR}"
